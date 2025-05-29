05f27eafd08df95322e80035639a74d5e706ea5c
Kristian Lyngbaek <klyngbaek@users.noreply.github.com>
2025-05-28 22:36:08 -0700

    Removed traces of SUAT

--- app/private_config.ts
+++ app/private_config.ts
@@ -3,10 +3,8 @@
 const privateConfig =
 {
     "fb_app_secret": process.env.FB_APP_SECRET,
-    "fb_suat": process.env.FB_SUAT,
     "fb_reg_pin": process.env.FB_REG_PIN,
-    "fb_verify_token": process.env.FB_VERIFY_TOKEN,
-    "fb_admin_suat": process.env.FB_ADMIN_SUAT,
+    "fb_verify_token": process.env.FB_VERIFY_TOKEN
 }
 
 export default async function getPrivateConfig() {
