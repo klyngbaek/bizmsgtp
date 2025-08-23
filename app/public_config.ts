e1b93f3febc4ddae787fe9fa15f275b8e5d0c706
Kristian Lyngbaek <klyngbaek@users.noreply.github.com>
2025-08-23 08:43:55 -0700

    Added new es versions

--- app/public_config.ts
+++ app/public_config.ts
@@ -8,29 +8,48 @@ const publicConfig =
     "graph_api_version": process.env.FB_GRAPH_API_VERSION,
     "tp_configs": (process.env.FB_TP_CONFIG_IDS ?? '').split(','),
     "public_es_feature_options": {
-        "v2": [],
+        "v2": [
+            "marketing_messages_lite"
+        ],
+        "v2-public-preview": [
+            "marketing_messages_lite",
+            "app_only_install"
+        ],
         "v3-alpha-1": [
             "marketing_messages_lite",
             "cloud_api",
             "conversions_api",
             "mm_mapi",
             "ctwa",
-            "api_access_only"
+            "ctm",
+            "ctd",
+            "api_access_only",
         ],
         "v3": [
             "marketing_messages_lite",
             "api_access_only",
         ],
+        "v3-public-preview": [
+            "marketing_messages_lite",
+            "api_access_only",
+        ]
     },
     "public_es_versions": [
         "v2",
+        "v2-public-preview",
         "v3-alpha-1",
         "v3",
+        "v3-public-preview",
     ],
     "public_es_feature_types": {
         "v2": [
             "whatsapp_business_app_onboarding",
             "only_waba_sharing"
+        ],
+        "v2-public-preview": [
+            "whatsapp_business_app_onboarding",
+            "only_waba_sharing",
+            "marketing_messages_lite"
         ],
         "v3-alpha-1": [
             "whatsapp_business_app_onboarding",
@@ -38,6 +57,9 @@ const publicConfig =
         "v3": [
             "whatsapp_business_app_onboarding",
             "api_access_only"
+        ],
+        "v3-public-preview": [
+            "whatsapp_business_app_onboarding"
         ],
     },
 };
